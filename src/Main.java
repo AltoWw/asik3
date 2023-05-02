2f214a69276179a5345ff1f4e98b87f77a7fd80c
Altynbek <rysbek20051@gmail.com>
2023-05-02 22:55:18 +0600

    main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyHashTable<MyTestingClass, Student> table = new MyHashTable<>();
+        Random r = new Random();
+        for (int i = 0; i < 10000; i++) {
+            MyTestingClass key = new MyTestingClass(i, "Name" + i);
+            Student value = new Student("Student" + i, r.nextInt(100));
+            table.put(key, value);
+        }
+        System.out.println(table);
+        for (int i = 0; i < table.getM(); i++) {
+            System.out.println("Bucket " + i + ": " + table.getBucketSize(i));
+        }
     }
 }
